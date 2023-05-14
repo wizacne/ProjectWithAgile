c5123a391cef0311a1b7780310f0534aff670227
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2023-05-14 17:53:46 +0700

    set up and install lib

---
+++ constants/Themes.js
@@ -0,0 +1,37 @@
+
+
+export const COLOR = {
+    primary: '#FF5E00',
+    title: '#7F4E1D',
+    description: '#7F4E1D',
+    buttonDisabled: '#DDDDDD',
+    iconNotFocused: '#6D3805'
+}
+
+export const SIZES = {
+    h1: 26,
+    h2: 24,
+    h3: 22,
+    h4: 20,
+    h5: 18,
+    h6: 16,
+
+}
+
+export const ICON = {
+    shop: require('../assets/icon/icons8-home-page-50.png'),
+    explore: require('../assets/icon/icons8-search-50.png'),
+    cart: require('../assets/icon/icons8-shopping-cart-50.png'),
+    favorites: require('../assets/icon/icons8-favorite-50.png'),
+    account: require('../assets/icon/icons8-user-50.png'),
+    visit: require('../assets/icon/icons8-visit-50.png'),
+
+
+
+    flagVietNam :  require('../assets/ImageFlag/vietnam.png'),
+}
+
+export const IMAGES = {
+
+}
+
